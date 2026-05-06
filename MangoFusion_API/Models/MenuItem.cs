9ba1c83eee384c170963469e823ffa3e87fef30d
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-06 17:32:50 +0100

    Section 12 - 203 - Seed Menu Items

--- MangoFusion_API/Models/MenuItem.cs
+++ MangoFusion_API/Models/MenuItem.cs
@@ -11,6 +11,7 @@ namespace MangoFusion_API.Models
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string SpecialTag { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
 
         [Range (1,1000)]
         public double Price { get; set; }
