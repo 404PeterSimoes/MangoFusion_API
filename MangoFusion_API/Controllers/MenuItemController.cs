03b2695354107e6e5b583cdd3ff77f96c682f52e
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-07 16:44:01 +0100

    Section 12 - 212 - Delete Menu Item

--- MangoFusion_API/Controllers/MenuItemController.cs
+++ MangoFusion_API/Controllers/MenuItemController.cs
@@ -61,7 +61,7 @@ namespace MangoFusion_API.Controllers
                     }
 
                     var imagesPath = Path.Combine(_env.WebRootPath, "images");
-                    if(!Directory.Exists(imagesPath))
+                    if (!Directory.Exists(imagesPath))
                     {
                         Directory.CreateDirectory(imagesPath);
                     }
@@ -74,7 +74,7 @@ namespace MangoFusion_API.Controllers
                     }
 
                     // uploading the image
-                    using(var stream = new FileStream(filePath, FileMode.Create))
+                    using (var stream = new FileStream(filePath, FileMode.Create))
                     {
                         await menuItemCreateDTO.File.CopyToAsync(stream);
                     }
@@ -86,7 +86,7 @@ namespace MangoFusion_API.Controllers
                         Category = menuItemCreateDTO.Category,
                         SpecialTag = menuItemCreateDTO.SpecialTag,
                         Price = menuItemCreateDTO.Price,
-                        Image = "images/"+ menuItemCreateDTO.File.FileName
+                        Image = "images/" + menuItemCreateDTO.File.FileName
                     };
 
                     _db.MenuItems.Add(menuItem);
@@ -120,7 +120,7 @@ namespace MangoFusion_API.Controllers
             {
                 if (ModelState.IsValid)
                 {
-                    if (menuItemUpdateDTO.File == null || menuItemUpdateDTO.Id!=id)
+                    if (menuItemUpdateDTO.File == null || menuItemUpdateDTO.Id != id)
                     {
                         _response.IsSuccess = false;
                         _response.StatusCode = HttpStatusCode.BadRequest;
@@ -171,6 +171,56 @@ namespace MangoFusion_API.Controllers
                     }
 
                     _db.MenuItems.Update(menuItemFromDb);
+                    await _db.SaveChangesAsync();
+
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    return Ok(_response);
+
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = [ex.ToString()];
+            }
+
+            return BadRequest(_response);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<ApiResponse>> DeleteMenuItem(int id)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (id == 0)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        return BadRequest(_response);
+                    }
+
+                    MenuItem? menuItemFromDb = await _db.MenuItems.FirstOrDefaultAsync(u => u.Id == id);
+
+                    if (menuItemFromDb == null)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        return NotFound(_response);
+                    }
+
+                    var filePath_OldFile = Path.Combine(_env.WebRootPath, menuItemFromDb.Image);
+                    if (System.IO.File.Exists(filePath_OldFile))
+                    {
+                        System.IO.File.Delete(filePath_OldFile);
+                    }
+
+                    _db.MenuItems.Remove(menuItemFromDb);
                     await _db.SaveChangesAsync();
 
                     _response.StatusCode = HttpStatusCode.NoContent;
