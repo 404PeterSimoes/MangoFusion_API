e4f8c96688244ccaf456b4cbb23d795f3e81b4dc
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-07 16:56:28 +0100

    Section 12 - 213 - Add Image for Menu Item

--- MangoFusion_API/Data/ApplicationDbContext.cs
+++ MangoFusion_API/Data/ApplicationDbContext.cs
@@ -23,7 +23,7 @@ namespace MangoFusion_API.Data
                 Id = 1,
                 Name = "Spring Roll",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "https://dotnetmasteryimages.blob.core.windows.net/redmango/spring roll.jpg",
+                Image = "images/spring roll.jpg",
                 Price = 7.99,
                 Category = "Appetizer",
                 SpecialTag = ""
@@ -32,7 +32,7 @@ namespace MangoFusion_API.Data
                 Id = 2,
                 Name = "Idli",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "https://dotnetmasteryimages.blob.core.windows.net/redmango/idli.jpg",
+                Image = "images/idli.jpg",
                 Price = 8.99,
                 Category = "Appetizer",
                 SpecialTag = ""
@@ -41,7 +41,7 @@ namespace MangoFusion_API.Data
                 Id = 3,
                 Name = "Panu Puri",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "https://dotnetmasteryimages.blob.core.windows.net/redmango/pani puri.jpg",
+                Image = "images/pani puri.jpg",
                 Price = 8.99,
                 Category = "Appetizer",
                 SpecialTag = "Best Seller"
@@ -50,7 +50,7 @@ namespace MangoFusion_API.Data
                 Id = 4,
                 Name = "Hakka Noodles",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "https://dotnetmasteryimages.blob.core.windows.net/redmango/hakka noodles.jpg",
+                Image = "images/hakka noodles.jpg",
                 Price = 10.99,
                 Category = "Entrée",
                 SpecialTag = ""
@@ -59,7 +59,7 @@ namespace MangoFusion_API.Data
                 Id = 5,
                 Name = "Malai Kofta",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "https://dotnetmasteryimages.blob.core.windows.net/redmango/malai kofta.jpg",
+                Image = "images/malai kofta.jpg",
                 Price = 12.99,
                 Category = "Entrée",
                 SpecialTag = "Top Rated"
@@ -68,7 +68,7 @@ namespace MangoFusion_API.Data
                 Id = 6,
                 Name = "Paneer Pizza",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "https://dotnetmasteryimages.blob.core.windows.net/redmango/paneer pizza.jpg",
+                Image = "images/paneer pizza.jpg",
                 Price = 11.99,
                 Category = "Entrée",
                 SpecialTag = ""
@@ -77,7 +77,7 @@ namespace MangoFusion_API.Data
                 Id = 7,
                 Name = "Paneer Tikka",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "https://dotnetmasteryimages.blob.core.windows.net/redmango/paneer tikka.jpg",
+                Image = "images/paneer tikka.jpg",
                 Price = 13.99,
                 Category = "Entrée",
                 SpecialTag = "Chef's Special"
@@ -86,7 +86,7 @@ namespace MangoFusion_API.Data
                 Id = 8,
                 Name = "Carrot Love",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "https://dotnetmasteryimages.blob.core.windows.net/redmango/carrot love.jpg",
+                Image = "images/carrot love.jpg",
                 Price = 4.99,
                 Category = "Dessert",
                 SpecialTag = ""
@@ -95,7 +95,7 @@ namespace MangoFusion_API.Data
                 Id = 9,
                 Name = "Rasmalai",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "https://dotnetmasteryimages.blob.core.windows.net/redmango/rasmalai.jpg",
+                Image = "images/rasmalai.jpg",
                 Price = 4.99,
                 Category = "Dessert",
                 SpecialTag = "Chef's Special"
@@ -104,7 +104,7 @@ namespace MangoFusion_API.Data
                 Id = 10,
                 Name = "Sweet Rolls",
                 Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-                Image = "https://dotnetmasteryimages.blob.core.windows.net/redmango/sweet rolls.jpg",
+                Image = "images/sweet rolls.jpg",
                 Price = 3.99,
                 Category = "Dessert",
                 SpecialTag = "Top Rated"
