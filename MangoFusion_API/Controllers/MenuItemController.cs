1f3e41b78189b578a778bd36e20591cf5c3314c2
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-07 12:13:54 +0100

    Section 12 - Create Menu Item

--- MangoFusion_API/Controllers/MenuItemController.cs
+++ MangoFusion_API/Controllers/MenuItemController.cs
@@ -1,7 +1,9 @@
 ﻿using MangoFusion_API.Data;
 using MangoFusion_API.Models;
+using MangoFusion_API.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Security.AccessControl;
 
 namespace MangoFusion_API.Controllers
 {
@@ -11,6 +13,7 @@ namespace MangoFusion_API.Controllers
     {
         private readonly ApplicationDbContext _db;
         private readonly ApiResponse _response;
+        private readonly IWebHostEnvironment _env;
         public MenuItemController(ApplicationDbContext db)
         {
             _db = db;
@@ -25,10 +28,11 @@ namespace MangoFusion_API.Controllers
             return Ok(_response);
         }
 
-        [HttpGet("{id:int}",Name ="GetMenuItem")]
+        [HttpGet("{id:int}", Name = "GetMenuItem")]
         public IActionResult GetMenuItem(int id)
         {
-            if (id == 0) {
+            if (id == 0)
+            {
                 _response.StatusCode = HttpStatusCode.BadRequest;
                 _response.IsSuccess = false;
                 return BadRequest(_response);
@@ -37,6 +41,72 @@ namespace MangoFusion_API.Controllers
             _response.Result = menuItem;
             _response.StatusCode = HttpStatusCode.OK;
             return Ok(_response);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> CreateMenuItem([FromForm] MenuItemCreateDTO menuItemCreateDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (menuItemCreateDTO.File == null || menuItemCreateDTO.File.Length == 0)
+                    {
+                        _response.IsSuccess = false;
+                        _response.StatusCode = HttpStatusCode.BadRequest;
+                        _response.ErrorMessages = ["File is required."];
+                        return BadRequest(_response);
+                    }
+
+                    var imagesPath = Path.Combine(_env.WebRootPath, "images");
+                    if(!Directory.Exists(imagesPath))
+                    {
+                        Directory.CreateDirectory(imagesPath);
+                    }
+
+                    var filePath = Path.Combine(imagesPath, menuItemCreateDTO.File.FileName);
+
+                    if (System.IO.File.Exists(filePath))
+                    {
+                        System.IO.File.Delete(filePath);
+                    }
+
+                    // uploading the image
+                    using(var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await menuItemCreateDTO.File.CopyToAsync(stream);
+                    }
+
+                    MenuItem menuItem = new MenuItem
+                    {
+                        Name = menuItemCreateDTO.Name,
+                        Description = menuItemCreateDTO.Description,
+                        Category = menuItemCreateDTO.Category,
+                        SpecialTag = menuItemCreateDTO.SpecialTag,
+                        Price = menuItemCreateDTO.Price,
+                        Image = "images/"+ menuItemCreateDTO.File.FileName
+                    };
+
+                    _db.MenuItems.Add(menuItem);
+                    await _db.SaveChangesAsync();
+
+                    _response.Result = menuItemCreateDTO;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return CreatedAtRoute("GetMenuItem", new { id = menuItem.Id }, _response);
+
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = [ex.ToString()];
+            }
+
+            return BadRequest(_response);
         }
     }
 }
