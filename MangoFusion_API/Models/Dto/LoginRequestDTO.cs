9e76c5719a2cf9dd78f81c1d7182d0ad9cd53116
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-11 17:05:49 +0100

    Section 13 - 221 - Login Endpoint

--- MangoFusion_API/Models/Dto/LoginRequestDTO.cs
+++ MangoFusion_API/Models/Dto/LoginRequestDTO.cs
@@ -2,7 +2,7 @@
 {
     public class LoginRequestDTO
     {
-        public string UserEmail { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
 }
