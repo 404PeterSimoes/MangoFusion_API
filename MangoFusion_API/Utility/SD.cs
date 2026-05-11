645557dfe9a2c93c945ae579e30b3389aac2564a
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-11 09:51:54 +0100

    Section 13 - 214 - Add Auth Test Controller

---
+++ MangoFusion_API/Utility/SD.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MangoFusion_API.Utility
+{
+    public static class SD
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Customer = "Customer";
+    }
+}
