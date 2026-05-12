b035324d5a933358748f9517a6978bde5f6f2e94
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-12 16:08:49 +0100

    Section 14 - 232 - Rating in Action

--- MangoFusion_API/Models/MenuItem.cs
+++ MangoFusion_API/Models/MenuItem.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MangoFusion_API.Models
 {
@@ -18,6 +19,9 @@ namespace MangoFusion_API.Models
 
         [Required]
         public string Image { get; set; } = string.Empty;
+
+        [NotMapped]
+        public double Rating { get; set; }
 
     }
 }
