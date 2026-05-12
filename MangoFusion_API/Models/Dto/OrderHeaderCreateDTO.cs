89bc329f460f6dbc22b5eb32c47de6faa4e3ac52
404PeterSimoes <176055646+404PeterSimoes@users.noreply.github.com>
2026-05-12 10:17:55 +0100

    Section 14 - 224 - Order Header and Details DTO

---
+++ MangoFusion_API/Models/Dto/OrderHeaderCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MangoFusion_API.Models.Dto
+{
+    public class OrderHeaderCreateDTO
+    {
+        [Required]
+        public string PickUpName { get; set; } = string.Empty;
+        [Required]
+        public string PickUpPhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public string PickUpEmail { get; set; } = string.Empty;
+
+        public double OrderTotal { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public int TotalItems { get; set; }
+
+        public List<OrderDetailsCreateDTO> OrderDetailsDTO { get; set; } = new();
+    }
+}
